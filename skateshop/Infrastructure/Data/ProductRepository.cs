b35d47d8e3b2b02d4d092d4f977115761cae2579
aaronrcs <aromer61@calstatela.edu>
2020-08-06 21:30:23 -0700

    Added additional methods to Products controller to acquire info about brands and products

--- skateshop/Infrastructure/Data/ProductRepository.cs
+++ skateshop/Infrastructure/Data/ProductRepository.cs
@@ -14,6 +14,11 @@ namespace Infrastructure.Data
             _context = context;
         }
 
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await _context.ProductBrands.ToListAsync();
+        }
+
         public async Task<Product> GetProductByIdAsync(int id)
         {
             return await _context.Products.FindAsync(id);
@@ -24,5 +29,9 @@ namespace Infrastructure.Data
             return await _context.Products.ToListAsync();
         }
 
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
+        }
     }
 }
