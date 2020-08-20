1e39d8638940396ec2ec9a0841dfeba60488888e
aaronrcs <aromer61@calstatela.edu>
2020-08-19 23:11:08 -0700

    Updated product.json file and re created a new migration to properly seed data into database

--- skateshop/Infrastructure/Data/Migrations/20200820060121_InitialCreate.Designer.cs
+++ skateshop/Infrastructure/Data/Migrations/20200820060121_InitialCreate.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20200803212709_InitialCreate")]
+    [Migration("20200820060121_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
