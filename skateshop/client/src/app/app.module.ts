3408ff8aa583fb4b91b9003ae19897cacac286aa
aaronrcs <aromer61@calstatela.edu>
2020-10-05 17:59:32 -0700

    Created Error components to deal with error handling and also utilized an interceptor to catch errors as well

--- skateshop/client/src/app/app.module.ts
+++ skateshop/client/src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { ErrorInterceptor } from './core/Interceptors/error.interceptor';
 import { HomeModule } from './home/home.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,9 @@ import { SharedModule } from './shared/shared.module';
     HomeModule
 
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
