a11ac261d1e2b7d23f72bea3524aa4f82a9b6bb7
aaronrcs <aromer61@calstatela.edu>
2020-10-28 21:18:07 -0700

    Added more helper functions in Basket Service

--- skateshop/client/src/app/basket/basket-cart.service.ts
+++ skateshop/client/src/app/basket/basket-cart.service.ts
@@ -1,3 +1,5 @@
+import { Basket, IBasketItem } from './../shared/models/basket';
+import { IProduct } from './../shared/models/product';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -34,5 +36,46 @@ export class BasketCartService {
 
   getCurrentBasketValue() {
     return this.basketSource.value;
+  }
+
+  addItemToBasket(item: IProduct, quantity = 1) {
+    const itemToAdd: IBasketItem = this.mapProductItemToBasketItem(item, quantity);
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+
+  // Helper private function to Add or Update current items in Shopping Cart
+  // Will check to see if the same item exists in the cart, if true, it will increase the quantity
+  // If false (meaning a new item is being added), it will simply add the new item to Shopping Cart
+  private addOrUpdateItem(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number): IBasketItem[] {
+    const index = items.findIndex(i => i.id === itemToAdd.id);
+    if (index === -1) {
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    } else {
+      items[index].quantity += quantity;
+    }
+    return items;
+  }
+
+  // Helper private function to create a new Basket Item
+  private createBasket(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  // Helper private function to map properties from IProduct to IBasketItem
+  private mapProductItemToBasketItem(item: IProduct, quantity: number): IBasketItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureUrl: item.pictureUrl,
+      quantity,
+      brand: item.productBrand,
+      type: item.productType
+    }
   }
 }
