a969b9c5dddf04204cec9628ae8b47f950e2ecd5
aaronrcs <aromer61@calstatela.edu>
2020-11-04 19:40:56 -0800

    Added delete and increment functions to handle functionality of updating the amount of quantity items in shopping cart

--- skateshop/client/src/app/basket/basket-cart.service.ts
+++ skateshop/client/src/app/basket/basket-cart.service.ts
@@ -49,6 +49,45 @@ export class BasketCartService {
     this.setBasket(basket);
   }
 
+  incrementItemQuantity(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex(x => x.id === item.id);
+    basket.items[foundItemIndex].quantity++;
+    this.setBasket(basket);
+  }
+
+  decrementItemQuantity(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex(x => x.id === item.id);
+    if (basket.items[foundItemIndex].quantity > 1) {
+      basket.items[foundItemIndex].quantity--;
+      this.setBasket(basket);
+    } else {
+      this.removeItemFromCart(item);
+    }
+
+  }
+  removeItemFromCart(item: IBasketItem) {
+    const basket = this.getCurrentBasketValue();
+    if (basket.items.some(x => x.id === item.id)) {
+      basket.items = basket.items.filter(i => i.id !== item.id);
+      if (basket.items.length > 0) {
+        this.setBasket(basket);
+      } else {
+        this.deleteCart(basket);
+      }
+    }
+  }
+  deleteCart(basket: IBasket) {
+    return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe(() => {
+      this.cartSource.next(null);
+      this.cartTotalSource.next(null);
+      localStorage.removeItem('basket_id');
+    }, error => {
+      console.log(error);
+    })
+  }
+
   private calculateCartTotals () {
     const basket = this.getCurrentBasketValue();
     const shipping = 0;
